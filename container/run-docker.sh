99927681422ed70f155678db3e4072145a71359d
MinMiguelM <cmiguelmg@gmail.com>
2018-02-10 18:44:25 -0500

    docker fixes

--- container/run-docker.sh
+++ container/run-docker.sh
@@ -5,9 +5,9 @@ cd /home/ubuntu/Fondo-DevOps/
 
 docker run -d -p 80:80 -p 443:443 \
 	--name fondo_web \
-	-v front/nginx.conf:/etc/nginx/nginx.conf \
-	-v front/static_files/:/usr/share/nginx/html/static_files \
-	-v certificates/web/:/etc/nginx/certs/web \
-	-v certificates/api/:/etc/nginx/certs/api \
+	-v $(pwd)/front/nginx.conf:/etc/nginx/nginx.conf \
+	-v $(pwd)/front/static_files/:/usr/share/nginx/html/static_files \
+	-v $(pwd)/certificates/web/:/etc/nginx/certs/web \
+	-v $(pwd)/certificates/api/:/etc/nginx/certs/api \
 	--net fondo_network \
 	fonweb_image:$1
