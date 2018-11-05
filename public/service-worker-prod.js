9131b79f216caa02f0fea4da54751cc488d79040
MinMiguelM <cmiguelmg@gmail.com>
2018-11-05 17:24:27 -0500

    badge update

--- public/service-worker-prod.js
+++ public/service-worker-prod.js
@@ -4,7 +4,7 @@ self.addEventListener('push', function(event) {
     const options = {
         body: content.body,
         icon: 'icons/logo_64.png',
-        badge: 'icons/logo_16.png'
+        badge: 'icons/badge.png'
     };
   
     event.waitUntil(self.registration.showNotification(title, options));
