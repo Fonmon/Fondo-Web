c8c21067c11bd882f3cbeb36a4ab08e4488218ec
MinMiguelM <cmiguelmg@gmail.com>
2018-11-10 11:23:29 -0500

    on notification click was added to close issue #5

--- public/service-worker-dev.js
+++ public/service-worker-dev.js
@@ -25,8 +25,18 @@ self.addEventListener('push', function(event) {
     const options = {
         body: content.body,
         icon: 'icons/logo_64.png',
-        badge: 'icons/badge.png'
+        badge: 'icons/badge.png',
+        data: content
     };
   
     event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    const target = event.notification.data.target;
+
+    event.waitUntil(
+        clients.openWindow(target)
+    )
 });
