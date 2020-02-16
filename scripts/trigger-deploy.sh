a4c489b604ef0f57b63b7904d8b3787c0904d831
MinMiguelM <cmiguelmg@gmail.com>
2020-02-16 16:19:40 -0500

    setting gcloud project

--- scripts/trigger-deploy.sh
+++ scripts/trigger-deploy.sh
@@ -2,6 +2,7 @@
 set -x
 
 PROJECT_ID=notificationstest-90976
+gcloud config set project $PROJECT_ID
 
 if [ $TRAVIS_BRANCH == 'master' ]; then
   HOST_API=api-fonmon.minagle.com
@@ -24,6 +25,6 @@ docker tag fonmon_web:latest us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
 gcloud -q auth configure-docker
 gcloud -q container images delete us.gcr.io/$PROJECT_ID/fonmon_web:$TAG --force-delete-tags
 docker push us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
-gcloud -q run deploy $SERVICE --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
+gcloud -q run deploy $SERVICE --allow-unauthenticated --max-instances=1 --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
 
 exit 0
