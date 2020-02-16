400363272f4773e7c0715711616f8c8c5a1dcb27
MinMiguelM <cmiguelmg@gmail.com>
2020-02-16 14:55:40 -0500

    final script to deploy new revision

--- scripts/trigger-deploy.sh
+++ scripts/trigger-deploy.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 set -x
 
-$HOST_APP=cloud-fonmon.minagle.com
+PROJECT_ID=notificationstest-90976
 
-echo $TRAVIS_BRANCH
+if [ $TRAVIS_BRANCH == 'master' ]; then
+  HOST_API=api-fonmon.minagle.com
+  TAG=stable
+  SERVICE=fonmonweb
+else
+  HOST_API=cloud-api-fonmon.minagle.com
+  TAG=dev
+  SERVICE=fonmonweb_dev
+fi
 
 cd $TRAVIS_BUILD_DIR/
-echo "export const HOST_APP = \"https://${HOST_APP}/\";" > src/utils/Constants.js
+echo "export const HOST_APP = \"https://${HOST_API}/\";" > src/utils/Constants.js
 GENERATE_SOURCEMAP=false npm run build
 
 cd $TRAVIS_BUILD_DIR/container
 docker build -t fonmon_web -f ./Dockerfile ..
-docker tag fonmon_web:latest us.gcr.io/notificationstest-90976/fonmon_web:dev
+docker tag fonmon_web:latest us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
 
 gcloud -q auth configure-docker
-docker push us.gcr.io/notificationstest-90976/fonmon_web:dev
+gcloud -q container images delete us.gcr.io/$PROJECT_ID/fonmon_web:$TAG --force-delete-tags
+docker push us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
+gcloud -q run deploy $SERVICE --platform managed --region us-central1 --image us.gcr.io/$PROJECT_ID/fonmon_web:$TAG
 
 exit 0
